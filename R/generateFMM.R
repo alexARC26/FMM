0e7a61cbea787935267b2a8cbeebed22b58e93b9
christianco97 <78907479+christianco97@users.noreply.github.com>
2021-03-15 13:11:48 +0100

    Revert "Revert "Merge with CRAN version 1.0.2.""
    This reverts commit 7a84f0dde260a9f5dfde517553c71784cd0a0824.

--- R/generateFMM.R
+++ R/generateFMM.R
@@ -1,15 +1,15 @@
 # Simulating data from FMM models
 #
 # Arguments:
-#    M: value of the intercept parameter M.
-#    A: value of the FMM wave amplitude parameter A.
-#       To simulate a FMM model with m components vector of length m.
-#    a: value of the FMM wave phase translation parameter alpha.
-#       To simulate a FMM model with m components vector of length m.
-#    b: value of the FMM wave phase translation parameter beta.
-#       To simulate a FMM model with m components vector of length m.
-#    w: value of the FMM wave phase translation parameter omega
-#       To simulate a FMM model with m components vector of length m.
+#     M: value of the intercept parameter M.
+#     A: value of the FMM wave amplitude parameter A.
+#        To simulate a FMM model with m components vector of length m.
+# alpha: value of the FMM wave phase translation parameter alpha.
+#        To simulate a FMM model with m components vector of length m.
+# beta:  value of the FMM wave phase translation parameter beta.
+#        To simulate a FMM model with m components vector of length m.
+# omega: value of the FMM wave phase translation parameter omega
+#        To simulate a FMM model with m components vector of length m.
 #    from: initial time point. Default 0.
 #    to: final time point. Default 2*pi.
 #    length.out: desired length of the simulation. Default 100.
@@ -19,10 +19,10 @@
 #    outvalues: TRUE when the simulated data are returned.
 #    sigmaNoise: Standard deviation of the gaussian noise to be added.
 #                Its default value is 0 equivalent to a simulation set-up without noise.
-generate_FMM <- function(M,A,a,b,w,from=0,to=2*pi,length.out=100,timePoints=seq(from,to,length=length.out),
+generateFMM <- function(M,A,alpha,beta,omega,from=0,to=2*pi,length.out=100,timePoints=seq(from,to,length=length.out),
                 plot=TRUE,outvalues=TRUE,sigmaNoise=0){
 
-  narg <- max(length(M),length(A),length(a),length(b),length(w))
+  narg <- max(length(M),length(A),length(alpha),length(beta),length(omega))
 
   if(length(M)>1){
     warning("M parameter should be a vector of length 1.
@@ -35,20 +35,20 @@ generate_FMM <- function(M,A,a,b,w,from=0,to=2*pi,length.out=100,timePoints=seq(
   A <- rep(A,length.out=narg)
   if(sum(A <= 0) > 0) stop("A parameter must be positive.")
 
-  a <- rep(a,length.out=narg)
-  a <- a%%(2*pi) # between 0 and 2*pi
+  alpha <- rep(alpha,length.out=narg)
+  alpha <- alpha%%(2*pi) # between 0 and 2*pi
 
-  b <- rep(b,length.out=narg)
-  b <- b%%(2*pi) # between 0 and 2*pi
+  beta <- rep(beta,length.out=narg)
+  beta <- beta%%(2*pi) # between 0 and 2*pi
 
-  w <- rep(w,length.out=narg)
-  if(sum(w<0)>0 | sum(w>1)>0) stop("w parameter must be between 0 and 1.")
+  omega <- rep(omega,length.out=narg)
+  if(sum(omega<0)>0 | sum(omega>1)>0) stop("omega parameter must be between 0 and 1.")
 
   t <- timePoints
 
   phi <- list()
   for(i in 1:narg){
-    phi[[i]] <- b[i]+2*atan(w[i]*tan((t-a[i])/2))
+    phi[[i]] <- beta[i]+2*atan(omega[i]*tan((t-alpha[i])/2))
   }
 
   ym <- list()
@@ -71,5 +71,5 @@ generate_FMM <- function(M,A,a,b,w,from=0,to=2*pi,length.out=100,timePoints=seq(
   }
 
 
-  if(outvalues) return(list(input=list(M = M[1]*narg, A=A,alpha=a,beta=b,omega=w),t=t,y=y))
+  if(outvalues) return(list(input=list(M = M[1]*narg, A=A,alpha=alpha,beta=beta,omega=omega),t=t,y=y))
 }
